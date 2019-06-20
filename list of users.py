e55be43120cd828d2ca89dacdaea1d4fd8368a85
ayushman2001 <46623283+ayushman2001@users.noreply.github.com>
2019-06-20 14:02:26 +0530

    Update and rename a.py to list of users.py

---
+++ list of users.py
@@ -0,0 +1,54 @@
+import urllib.request
+import json
+import pandas as pd
+
+
+f=open(r"E:\CC rank predictor\New Text Document.txt", "w",encoding="utf-8")
+a1="https://www.codechef.com/api/ratings/all?page="
+b1="&sortBy=rating&order=desc&itemsPerPage=100"
+flag=1
+t=1
+
+#making of headers
+html_file1=urllib.request.Request("https://www.codechef.com/api/ratings/all?page=1&sortBy=rating&order=desc&itemsPerPage=100", headers={"User-Agent" :"Mozilla /5.0"})
+with urllib.request.urlopen(html_file1) as url1:
+    data1 = json.loads(url1.read().decode())
+#    print(data1)
+data1=data1["list"]
+attributes=list(data1[0].keys())
+attributes.pop()
+headers=""
+for i in range(len(attributes)):
+    
+    headers=headers+attributes[i]+","
+f.write(headers+"\n")
+
+
+
+
+err=0
+
+
+while(t<4420):
+    try:
+        html_file=urllib.request.Request(a1+str(t)+b1, headers={"User-Agent" :"Mozilla /5.0"})
+        with urllib.request.urlopen(html_file) as url:
+            data = json.loads(url.read().decode())
+    #        print(data)
+        data=data["list"]
+        attributes=list(data[0].keys())
+        attributes.pop()
+        headers=""
+        
+        for i in range(len(data)):
+            a=data[i]
+            tempstr=""
+            for att in attributes:
+                tempstr=tempstr+(str(a[att])).replace(","," ")+","
+#            print(tempstr+"\n")
+            f.write(tempstr+"\n")
+        t=t+1
+    except:
+        err=err+1
+        
+    
